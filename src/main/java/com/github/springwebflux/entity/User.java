90e310d2da2d8cbb6d0ab593262602b34a4f35f2
pjx <lingxiao@2dfire.com>
2018-09-17 23:29:50 +0800

    user

---
+++ src/main/java/com/github/springwebflux/entity/User.java
@@ -0,0 +1,29 @@
+package com.github.springwebflux.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午11:19 2018/9/17
+ * @desc
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class User {
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    private String name;
+    private String username;
+    private String phone;
+    private Date Birthday;
+}
--- src/main/java/com/github/springwebflux/reposity/BookReposity.java
+++
@@ -1,9 +0,0 @@
-package com.github.springwebflux.reposity;
-
-/**
- * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
- * @time: Created in 下午5:49 2018/9/15
- * @desc
- */
-public interface BookReposity  {
-}
