abca12587397c9ad9fdc0c67fe6294b72bfb41dd
pjx <lingxiao@2dfire.com>
2018-09-28 14:37:02 +0800

    bean parser

---
+++ src/main/java/com/github/springwebflux/spring/SpringWebfluxRegistryParser.java
@@ -0,0 +1,18 @@
+package com.github.springwebflux.spring;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午2:35 2018/9/28
+ * @desc
+ */
+public class SpringWebfluxRegistryParser implements BeanDefinitionParser {
+    @Override
+    public BeanDefinition parse(Element element, ParserContext parserContext) {
+        return null;
+    }
+}
---
+++ src/main/java/com/github/springwebflux/spring/SpringWebfluxServiceParser.java
@@ -0,0 +1,19 @@
+package com.github.springwebflux.spring;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午2:36 2018/9/28
+ * @desc
+ */
+public class SpringWebfluxServiceParser implements BeanDefinitionParser {
+
+    @Override
+    public BeanDefinition parse(Element element, ParserContext parserContext) {
+        return null;
+    }
+}
