8d5fd4c9a04b1abeaa10b4e25e10d5e50461eb47
pjx <lingxiao@2dfire.com>
2018-09-28 14:57:01 +0800

    SpringWebfluxReferenceParser 实现

--- src/main/java/com/github/springwebflux/spring/SpringWebfluxReferenceParser.java
+++ src/main/java/com/github/springwebflux/spring/SpringWebfluxReferenceParser.java
@@ -1,6 +1,7 @@
 package com.github.springwebflux.spring;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
@@ -11,8 +12,24 @@ import org.w3c.dom.Element;
  * @desc
  */
 public class SpringWebfluxReferenceParser implements BeanDefinitionParser {
+
+
     @Override
     public BeanDefinition parse(Element element, ParserContext parserContext) {
-        return null;
+        String interfaceName = element.getAttribute("interfaceName");
+        String id = element.getAttribute("id");
+        String ipAddress = element.getAttribute("ipAddress");
+        String protocolType = element.getAttribute("protocol");
+
+        RootBeanDefinition beanDefinition = new RootBeanDefinition();
+        beanDefinition.setBeanClass(SpringWebfluxReference.class);
+        beanDefinition.setLazyInit(false);
+
+        beanDefinition.getPropertyValues().addPropertyValue("interfaceName", interfaceName);
+        beanDefinition.getPropertyValues().addPropertyValue("ipAddress", ipAddress);
+        beanDefinition.getPropertyValues().addPropertyValue("protocol", protocolType);
+
+        parserContext.getRegistry().registerBeanDefinition(id, beanDefinition);
+        return beanDefinition;
     }
 }
