97d0f0466c825e0fe66ff386d520978b8fc90d41
pjx <lingxiao@2dfire.com>
2018-09-28 14:59:29 +0800

    SpringWebfluxService 实现

--- src/main/java/com/github/springwebflux/spring/SpringWebfluxRegistryParser.java
+++ src/main/java/com/github/springwebflux/spring/SpringWebfluxRegistryParser.java
@@ -1,6 +1,7 @@
 package com.github.springwebflux.spring;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
@@ -13,6 +14,18 @@ import org.w3c.dom.Element;
 public class SpringWebfluxRegistryParser implements BeanDefinitionParser {
     @Override
     public BeanDefinition parse(Element element, ParserContext parserContext) {
-        return null;
+        String id = element.getAttribute("id");
+        String ipAddr = element.getAttribute("ipAddr");
+        String echoApiPort = element.getAttribute("echoApiPort");
+        String protocolType = element.getAttribute("protocol");
+
+        RootBeanDefinition beanDefinition = new RootBeanDefinition();
+        beanDefinition.setBeanClass(SpringWebfluxRegistry.class);
+        beanDefinition.getPropertyValues().addPropertyValue("ipAddr", ipAddr);
+        beanDefinition.getPropertyValues().addPropertyValue("echoApiPort", echoApiPort);
+        beanDefinition.getPropertyValues().addPropertyValue("protocol", protocolType);
+        parserContext.getRegistry().registerBeanDefinition(id, beanDefinition);
+
+        return beanDefinition;
     }
 }
--- src/main/java/com/github/springwebflux/spring/SpringWebfluxServiceParser.java
+++ src/main/java/com/github/springwebflux/spring/SpringWebfluxServiceParser.java
@@ -1,6 +1,7 @@
 package com.github.springwebflux.spring;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
@@ -14,6 +15,19 @@ public class SpringWebfluxServiceParser implements BeanDefinitionParser {
 
     @Override
     public BeanDefinition parse(Element element, ParserContext parserContext) {
-        return null;
+        String interfaceName = element.getAttribute("interfaceName");
+        String ref = element.getAttribute("ref");
+        String filter = element.getAttribute("filter");
+
+        RootBeanDefinition beanDefinition = new RootBeanDefinition();
+        beanDefinition.setBeanClass(SpringWebfluxService.class);
+        beanDefinition.setLazyInit(false);
+        beanDefinition.getPropertyValues().addPropertyValue("interfaceName", interfaceName);
+        beanDefinition.getPropertyValues().addPropertyValue("ref", ref);
+        beanDefinition.getPropertyValues().addPropertyValue("filter", filter);
+
+        parserContext.getRegistry().registerBeanDefinition(interfaceName, beanDefinition);
+
+        return beanDefinition;
     }
 }
