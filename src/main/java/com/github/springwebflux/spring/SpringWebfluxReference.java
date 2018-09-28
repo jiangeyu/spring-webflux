efed7adb19991f51d1f6a86743923594e6429786
pjx <lingxiao@2dfire.com>
2018-09-28 15:17:16 +0800

    SpringWebfluxReference

--- src/main/java/com/github/springwebflux/spring/SpringWebfluxReference.java
+++ src/main/java/com/github/springwebflux/spring/SpringWebfluxReference.java
@@ -1,10 +1,15 @@
 package com.github.springwebflux.spring;
 
-import com.google.common.eventbus.EventBus;
+import com.google.common.reflect.AbstractInvocationHandler;
+import com.google.common.reflect.Reflection;
 import lombok.Builder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
+
+import java.lang.reflect.Method;
 
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
@@ -12,7 +17,9 @@ import org.springframework.beans.factory.InitializingBean;
  * @desc
  */
 @Builder
-public class SpringWebfluxReference implements FactoryBean,InitializingBean,DisposableBean {
+public class SpringWebfluxReference implements FactoryBean, InitializingBean, DisposableBean {
+
+    private static Logger logger = LoggerFactory.getLogger(SpringWebfluxReference.class);
 
     private String interfaceName;
 
@@ -20,26 +27,34 @@ public class SpringWebfluxReference implements FactoryBean,InitializingBean,Disp
 
     private String protocol;
 
-    private EventBus eventBus = new EventBus();
-
 
     @Override
     public void destroy() throws Exception {
-
+        logger.info("destroy reference");
     }
 
     @Override
     public Object getObject() throws Exception {
-        return null;
+        return Reflection.newProxy(getObjectType(), new AbstractInvocationHandler() {
+            @Override
+            protected Object handleInvocation(Object proxy, Method method, Object[] args) throws Throwable {
+                return null;
+            }
+        });
     }
 
     @Override
     public Class<?> getObjectType() {
+        try {
+            return this.getClass().getClassLoader().loadClass(interfaceName);
+        } catch (ClassNotFoundException e) {
+            logger.error(e.toString());
+        }
         return null;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-
+        logger.info("after reference property set");
     }
 }
