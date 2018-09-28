0bb03fcaea547f97d367017b0dedfb77ae8df806
pjx <lingxiao@2dfire.com>
2018-09-28 14:20:19 +0800

    自定义命名空间解析

---
+++ src/main/java/com/github/springwebflux/spring/SpringWebfluxNamespaceHandler.java
@@ -0,0 +1,16 @@
+package com.github.springwebflux.spring;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午1:57 2018/9/28
+ * @desc
+ */
+public class SpringWebfluxNamespaceHandler extends NamespaceHandlerSupport {
+
+    @Override
+    public void init() {
+
+    }
+}
