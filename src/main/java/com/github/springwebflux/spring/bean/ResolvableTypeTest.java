15d2a7dbe3ba8f8236dd3ae92d4a8f253c7f8273
pjx <lingxiao@2dfire.com>
2018-10-16 18:38:43 +0800

    ResolvableType 测试

--- src/main/java/com/github/springwebflux/spring/bean/ResolvableTypeTest.java
+++ src/main/java/com/github/springwebflux/spring/bean/ResolvableTypeTest.java
@@ -30,5 +30,6 @@ public class ResolvableTypeTest {
         System.out.println(resolvableType.getGeneric(1));
         System.out.println(resolvableType.resolveGeneric( 0,1));
         System.out.println(resolvableType.getComponentType().toString());
+        System.out.println(resolvableType.hashCode());
     }
 }
