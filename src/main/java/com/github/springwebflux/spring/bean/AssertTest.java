c7ab4070ac3cf42490659e5dbab817bba834d7a8
pjx <lingxiao@2dfire.com>
2018-10-22 22:37:39 +0800

    assert test

---
+++ src/main/java/com/github/springwebflux/spring/bean/AssertTest.java
@@ -0,0 +1,15 @@
+package com.github.springwebflux.spring.bean;
+
+import org.springframework.util.Assert;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午10:36 2018/10/22
+ * @desc
+ */
+public class AssertTest {
+
+    public static void main(String[] args) {
+        Assert.isTrue(false,"hello");
+    }
+}
