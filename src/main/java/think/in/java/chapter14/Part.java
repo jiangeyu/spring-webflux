4a3abb708ca6ce3752ab0d4caeb46914403a043b
zhouchen <jiaxue.pjx@alibaba-inc.com>
2019-03-27 16:35:37 +0800

    注册工厂

---
+++ src/main/java/think/in/java/chapter14/Part.java
@@ -0,0 +1,28 @@
+package think.in.java.chapter14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
+ * @Description:
+ * @Date: Created in 下午4:19 2019/3/27
+ */
+public class Part {
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName();
+    }
+
+    static List<Factory<? extends Part>> partFactories = new ArrayList<>();
+
+    static {
+        partFactories.add(new FuelFilter.Factory());
+    }
+
+
+    public static void main(String[] args) {
+        partFactories.stream().forEach(f -> System.out.println(f.toString()));
+    }
+}
