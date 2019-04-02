a5f3f12cbb42273af22f8e80a5d2b60cabda5bb0
zhouchen <jiaxue.pjx@alibaba-inc.com>
2019-04-02 08:48:14 +0800

    数据存储恢复

---
+++ src/main/java/think/in/java/chapter18/BasicFileOutput.java
@@ -0,0 +1,20 @@
+package think.in.java.chapter18;
+
+import java.io.*;
+
+/**
+ * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
+ * @Description:
+ * @Date: Created in 上午8:25 2019/4/2
+ */
+public class BasicFileOutput {
+
+    static String file = "BasicFileOutput.out";
+
+//    public static void main(String[] args) throws FileNotFoundException {
+//        BufferedReader in = new BufferedReader(
+//                new StringReader(
+//                      new BufferedInputStream(
+//                              new FileInputStream(""))  ));
+//    }
+}
