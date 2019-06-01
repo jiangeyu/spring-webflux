b70ae7c2c6b241a00fc969ed70d0dac1acffaf0f
pjx <lingxiao@2dfire.com>
2019-06-01 15:28:51 +0800

    堆排序实现

--- data-structure/src/main/java/com/github/sort/SortDemo.java
+++ data-structure/src/main/java/com/github/sort/SortDemo.java
@@ -1,5 +1,7 @@
 package com.github.sort;
 
+import java.util.Arrays;
+
 /**
  * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
  * @Description:
@@ -7,37 +9,56 @@ package com.github.sort;
  */
 public class SortDemo {
 
-    public static void heapSort(int[] array) {
-        for (int i = array.length / 2; i > 0; i--) {
-            heapAdjust(array, i, array.length);
+    public static void main(String []args){
+        int []arr = {9,8,7,6,5,4,3,2,1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void sort(int []arr){
+        //1.构建大顶堆
+        for(int i=arr.length/2-1;i>=0;i--){
+            //从第一个非叶子结点从下至上，从右至左调整结构
+            adjustHeap(arr,i,arr.length);
+        }
+        //2.调整堆结构+交换堆顶元素与末尾元素
+        for(int j=arr.length-1;j>0;j--){
+            swap(arr,0,j);//将堆顶元素与末尾元素进行交换
+            adjustHeap(arr,0,j);//重新对堆进行调整
         }
 
-        for (int j = array.length; j > 0; j--) {
-            swap(array, 1, j);
-            heapAdjust(array, 1, j - 1);
-        }
     }
 
-    public static void heapAdjust(int[] a, int s, int m) {
-        int temp, j;
-        temp = a[0];
-        for (j = 2 * s; j <= m; j *= 2) {
-            if (j < m && a[j] > a[j + 1]) {
-                ++j;
+    /**
+     * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+     * @param arr
+     * @param i
+     * @param length
+     */
+    public static void adjustHeap(int []arr,int i,int length){
+        int temp = arr[i];//先取出当前元素i
+        for(int k=i*2+1;k<length;k=k*2+1){//从i结点的左子结点开始，也就是2i+1处开始
+            if(k+1<length && arr[k]<arr[k+1]){//如果左子结点小于右子结点，k指向右子结点
+                k++;
             }
-            if (temp > a[j]) {
+            if(arr[k] >temp){//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+                arr[i] = arr[k];
+                i = k;
+            }else{
                 break;
             }
-            a[s] = a[j];
-            s = j;
         }
-        a[s] = temp;
+        arr[i] = temp;//将temp值放到最终的位置
     }
 
-    public static void swap(int[] a, int i, int j) {
-        int temp;
-        temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+    /**
+     * 交换元素
+     * @param arr
+     * @param a
+     * @param b
+     */
+    public static void swap(int []arr,int a ,int b){
+        int temp=arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
