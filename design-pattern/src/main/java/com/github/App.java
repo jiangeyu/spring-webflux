cecd6611fe18a5f889ec94eaddaaf12945514a8d
zhouchen <jiaxue.pjx@alibaba-inc.com>
2019-06-07 22:02:11 +0800

    观察者模式observer定义

---
+++ design-pattern/src/main/java/com/github/App.java
@@ -0,0 +1,9 @@
+package com.github;
+
+/**
+ * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
+ * @Description:
+ * @Date: Created in 下午10:00 2019/6/7
+ */
+public class App {
+}
---
+++ design-pattern/src/main/java/com/github/oberver/WeatherType.java
@@ -0,0 +1,9 @@
+package com.github.oberver;
+
+/**
+ * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
+ * @Description:
+ * @Date: Created in 下午10:01 2019/6/7
+ */
+public class WeatherType {
+}
