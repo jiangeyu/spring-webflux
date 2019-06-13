a37d2b3c70863ab24a9a90922df80d8805b7e858
zhouchen <jiaxue.pjx@alibaba-inc.com>
2019-06-13 23:47:54 +0800

    event bus实现

--- CQRS/src/main/java/com/github/Storage/Bus.java
+++ CQRS/src/main/java/com/github/Storage/Bus.java
@@ -1,7 +1,11 @@
 package com.github.Storage;
 
 import com.github.Commands.Command;
+import com.github.Exceptions.UnregisteredDomainCommandException;
+import com.github.Utils.Converter;
+import org.aspectj.bridge.ICommand;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,13 +18,32 @@ import java.util.Map;
 public class Bus implements ICommandSender {
 
 
-    public Map<Command, List<Command> > handlerMap = new HashMap;
+    public Map<ICommandSender, List<ICommandSender> > handlerMap = new HashMap;
 
 
-    public void registerHandler();
+    public void registerHandler(List<ICommandSender> list) {
+
+        for(ICommandSender iCommandSender:list) {
+            if(handlerMap.containsKey(ICommandSender.class)) {
+                handlerMap.get(iCommandSender).add(iCommandSender);
+            } else {
+                List<ICommandSender> iCommandSenders = new ArrayList<>();
+                iCommandSenders.add(iCommandSender);
+                handlerMap.put(iCommandSender, iCommandSenders);
+            }
+        }
+    }
 
     @Override
     public void send(Command command) {
+        List<ICommandSender> list = new ArrayList<>();
+        if(list.contains(command)) {
+            list.get(0).send(command);
+        } else {
+            throw new UnregisteredDomainCommandException("");
+        }
 
     }
+
+
 }
