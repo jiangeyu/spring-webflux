450fb14db08d390f70ae5d2966202cdbb0e1d713
zhouchen <jiaxue.pjx@alibaba-inc.com>
2019-11-01 22:26:07 +0800

    桶限流

--- guava/src/main/java/com/github/guava/concurrent/Bucket.java
+++ guava/src/main/java/com/github/guava/concurrent/Bucket.java
@@ -1,9 +1,25 @@
 package com.github.guava.concurrent;
 
+import com.google.common.util.concurrent.Monitor;
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
  * @Description:
  * @Date: Created in 下午6:45 2019/10/31
  */
 public class Bucket {
+
+    private final ConcurrentLinkedQueue<Integer> container = new ConcurrentLinkedQueue<>();
+
+    private final static int BUCKET_LIMIT = 1000;
+
+    private final RateLimiter limiter = RateLimiter.create(10);
+
+    private final Monitor offerMonitor = new Monitor();
+    private final Monitor pollMonitor = new Monitor();
+
+
 }
