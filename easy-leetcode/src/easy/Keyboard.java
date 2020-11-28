8c9f61b1616a15637187bc52fc3168c36cf10b5e
zhouchen <jiaxue.pjx@alibaba-inc.com>
2020-11-28 21:00:21 +0800

    Leetcode

--- easy-leetcode/src/easy/Keyboard.java
+++ easy-leetcode/src/easy/Keyboard.java
@@ -26,15 +26,35 @@ public class Keyboard {
     public static int calculateTime2(String keyboard, String word) {
         int start = keyboard.indexOf(word.charAt(0));
 
-        for (int j = 1; j < word.length()-1; j++) {
+        for (int j = 1; j < word.length() - 1; j++) {
             int step = Math.abs(word.charAt(j + 1) - word.charAt(j));
             start += Math.abs(step);
         }
         return start;
     }
 
+    public static int reversePairs(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (new Long(nums[i]) > new Long(nums[j]) * 2) {
+                    sum += 1;
+                }
+            }
+        }
+
+        return sum;
+
+    }
+
     public static void main(String[] args) {
         System.out.println(calculateTime("pqrstuvwxyzabcdefghijklmno", "leetcode"));
         System.out.println(calculateTime("abcdefghijklmnopqrstuvwxyz", "cba"));
+
+//        System.out.println(reversePairs(new int[] {1,3,2,3,1}));
+//        System.out.println(reversePairs(new int[] {2,4,3,5,1}));
+        System.out.println(reversePairs(new int[]{2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2147483647}));
     }
 }
