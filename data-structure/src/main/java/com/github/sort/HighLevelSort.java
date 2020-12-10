91e51bf5bc1e9bf9654ab3218a66a645ee4942cd
zhouchen <jiaxue.pjx@alibaba-inc.com>
2020-12-10 20:31:00 +0800

    堆排序

--- data-structure/src/main/java/com/github/sort/HighLevelSort.java
+++ data-structure/src/main/java/com/github/sort/HighLevelSort.java
@@ -39,12 +39,17 @@ public class HighLevelSort {
         //建立父节点指标和子节点指标
         int dad = start;
         int son = dad * 2 + 1;
-        while (son <= end) { //若子节点指标在范围内才做比较
-            if (son + 1 <= end && num[son] < num[son + 1]) //先比较两个子节点大小，选择最大的
+        while (son <= end) {
+            //若子节点指标在范围内才做比较
+            if (son + 1 <= end && num[son] < num[son + 1]) {
+                //先比较两个子节点大小，选择最大的
                 son++;
-            if (num[dad] > num[son]) //如果父节点大于子节点代表调整完毕，直接跳出函数
+            }
+            if (num[dad] > num[son]) {
+                //如果父节点大于子节点代表调整完毕，直接跳出函数
                 return;
-            else { //否则交换父子内容再继续子节点和孙节点比较
+            } else {
+                //否则交换父子内容再继续子节点和孙节点比较
                 swap(dad, son);
                 dad = son;
                 son = dad * 2 + 1;
@@ -159,11 +164,11 @@ public class HighLevelSort {
 
 
     public static void main(String[] args) {
-        int[] num = {1, 9, 8, 2, 6};
+        int[] num = {3,5,3,0,8,6,1,5,8,6,2,4,9,4,7,0,1,8,9,7,3,1,2,5,9,7,4,0,2,6};
 
         HighLevelSort highLevelSort = new HighLevelSort(num);
 //
-        highLevelSort.heapSort(5);
+        highLevelSort.heapSort(30);
 //
 //        highLevelSort.recMergeSort(new int[20], 0, 4);
         System.out.println(num.toString());
