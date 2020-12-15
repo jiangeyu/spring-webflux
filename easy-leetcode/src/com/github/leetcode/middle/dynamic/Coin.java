ef90260fa5b554d1efdaa5d8a77208b5f0cb3230
zhouchen <jiaxue.pjx@alibaba-inc.com>
2020-12-15 10:52:13 +0800

    状态迁移

--- easy-leetcode/src/com/github/leetcode/middle/dynamic/Coin.java
+++ easy-leetcode/src/com/github/leetcode/middle/dynamic/Coin.java
@@ -70,6 +70,24 @@ public class Coin {
         return dp[n] > Integer.MAX_VALUE ? dp[n] % 1000000007 : dp[n];
     }
 
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     * 背包问题：看数组中是否存在加起来为sum/2的数.
+     * 背包容量（V） = sum/2
+     * 每一个物品只能装一次,如果出现背包中重量等于sum/2则为true else false
+     * dp[i]表示能否填满容量为i的背包
+     * 状态转移方程为 dp[i] = dp[i-1] || nums[i]+dp[i-nums[j]]
+     * 举例:v = sum/2 = 11   nums = [1,5,11,5]  1是true 0 是false
+     *          0  1  2  3  4  5  6  7  8  9  10  11
+     *  nums[0] 0  1  0  0  0  0  0  0  0  0   0   0
+     *  nums[1] 0  1  0  0  0  1  1  0  0  0   0   0
+     *  nums[2] 0  1  0  0  0  1  1  0  0  0   0   1
+     *  nums[3] 0  1  0  0  0  1  1  0  0  0   0   1
+     *
+     * 所以返回true，因为背包中nums[i]的状态都是由上一行决定的因此可以将二维转化为1维数组从尾部开始
+     */
+
     public static boolean canPartition(int[] nums) {
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
