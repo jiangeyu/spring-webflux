0be07a7acb38ec99488d9efc07c2bcbcc77117f5
zhouchen <jiaxue.pjx@alibaba-inc.com>
2020-12-21 09:28:50 +0800

    最长无重复子串

--- easy-leetcode/src/com/github/leetcode/middle/dynamic/Window.java
+++ easy-leetcode/src/com/github/leetcode/middle/dynamic/Window.java
@@ -1,5 +1,6 @@
 package com.github.leetcode.middle.dynamic;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -59,15 +60,16 @@ public class Window {
 
     /**
      * 给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的排列。
-     *
+     * <p>
      * 换句话说，第一个字符串的排列之一是第二个字符串的子串。
-     *
+     * <p>
      * 输入: s1 = "ab" s2 = "eidbaooo"
      * 输出: True
      * 解释: s2 包含 s1 的排列之一 ("ba").
-     *
+     * <p>
      * 输入: s1= "ab" s2 = "eidboaoo"
      * 输出: False
+     *
      * @param s1
      * @param s2
      * @return
@@ -111,42 +113,121 @@ public class Window {
 
     /**
      * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
-     *
+     * <p>
      * 字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
-     *
+     * <p>
      * 输入:
      * s: "cbaebabacd" p: "abc"
-     *
+     * <p>
      * 输出:
      * [0, 6]
-     *
+     * <p>
      * 解释:
      * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的字母异位词。
      * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的字母异位词。
-     *
-     *
+     * <p>
+     * <p>
      * 输入:
      * s: "abab" p: "ab"
-     *
+     * <p>
      * 输出:
      * [0, 1, 2]
-     *
+     * <p>
      * 解释:
      * 起始索引等于 0 的子串是 "ab", 它是 "ab" 的字母异位词。
      * 起始索引等于 1 的子串是 "ba", 它是 "ab" 的字母异位词。
      * 起始索引等于 2 的子串是 "ab", 它是 "ab" 的字母异位词。
-     *
+     * <p>
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode-cn.com/problems/find-all-anagrams-in-a-string
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-     *
      *
      * @param s
      * @param p
      * @return
      */
-    public List<Integer> findAnagrams(String s, String p) {
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        Map<Character, Integer> window = new HashMap<>();
+        Map<Character, Integer> need = new HashMap<>();
+        int minLength = p.length();
+        for (Character c : p.toCharArray()) {
+            need.compute(c, (k, v) -> need.get(k) == null ? 1 : need.get(k) + 1);
+        }
+        int left = 0, right = 0;
+        int length = Integer.MAX_VALUE;
+        int valid = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (need.containsKey(c)) {
+                window.compute(c, (k, v) -> window.get(c) == null ? 1 : window.get(c) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+            while (valid == need.size()) {
+                length = right - left;
+                if (length == minLength) {
+                    result.add(left);
+                }
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
 
+        }
+        return result;
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> window = new HashMap<>();
+        int right = 0;
+        int left = 0;
+        int res = 0;
+        int length = s.length();
+        while (right < length) {
+            char c = s.charAt(right);
+            right++;
+            window.compute(c, (k, v) -> window.get(c) == null ? 1 : window.get(c) + 1);
+            while (window.get(c) != null && window.get(c) > 1) {
+                char d = s.charAt(left);
+                left++;
+                window.put(d, window.get(d) - 1);
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring1(String s) {
+        // 记录字符上一次出现的位置
+        int[] last = new int[128];
+        for(int i = 0; i < 128; i++) {
+            last[i] = -1;
+        }
+        int n = s.length();
+
+        int res = 0;
+        int start = 0; // 窗口开始位置
+        for(int i = 0; i < n; i++) {
+            int index = s.charAt(i);
+            start = Math.max(start, last[index] + 1);
+            res   = Math.max(res, i - start + 1);
+            last[index] = i;
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
@@ -155,10 +236,13 @@ public class Window {
 //        map.compute('c', (k, v) -> map.get(k) == null ? 1 : map.get(k) + 1);
 //        map.compute('c', (k, v) -> map.get(k) + 1);
 //        System.out.println(map.get('c'));
-        System.out.println(checkInclusion("ab", "eidbaooo"));
-        System.out.println(checkInclusion("ab", "eidboaoo"));
+//        System.out.println(checkInclusion("ab", "eidbaooo"));
+//        System.out.println(checkInclusion("ab", "eidboaoo"));
 
 
 //        System.out.println(minWindow("BANCADOBECODE", "ABC"));
+//        System.out.println(findAnagrams("cbaebabacd", "abc"));
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring1("abcabcbb"));
     }
 }
