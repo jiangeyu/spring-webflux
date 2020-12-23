3fa5279ab9b1e3752af7b752f85413f27899efb2
zhouchen <jiaxue.pjx@alibaba-inc.com>
2020-12-23 23:10:04 +0800

    最长递增子序列

--- easy-leetcode/src/main/java/middle/dynamic/SubSequence.java
+++ easy-leetcode/src/main/java/middle/dynamic/SubSequence.java
@@ -42,16 +42,44 @@ public class SubSequence {
                 }
             }
             ans = Math.max(ans, dp[i]);
-
         }
         return ans;
     }
 
+    public static int lengthOfLIS11(int[] nums) {
+        /**
+         dp[i]: 所有长度为i+1的递增子序列中, 最小的那个序列尾数.
+         由定义知dp数组必然是一个递增数组, 可以用 maxL 来表示最长递增子序列的长度.
+         对数组进行迭代, 依次判断每个数num将其插入dp数组相应的位置:
+         1. num > dp[maxL], 表示num比所有已知递增序列的尾数都大, 将num添加入dp
+         数组尾部, 并将最长递增序列长度maxL加1
+         2. dp[i-1] < num <= dp[i], 只更新相应的dp[i]
+         **/
+        int maxL = 0;
+        int[] dp = new int[nums.length];
+        for (int num : nums) {
+            // 二分法查找, 也可以调用库函数如binary_search
+            int lo = 0, hi = maxL;
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (dp[mid] < num)
+                    lo = mid + 1;
+                else
+                    hi = mid;
+            }
+            dp[lo] = num;
+            if (lo == maxL)
+                maxL++;
+        }
+        return maxL;
+    }
+
     public static void main(String[] args) {
-        System.out.println(lengthOfLIS(new int[]{1, 5, 3, 6, 10, 9, 10}));
-        System.out.println(lengthOfLIS(new int[]{1, 2, 4, 5}));
-        System.out.println(lengthOfLIS(new int[]{1, 2, 6, 5}));
-        System.out.println(lengthOfLIS(new int[]{4, 10, 4, 3, 8, 9}));
+//        System.out.println(lengthOfLIS(new int[]{1, 5, 3, 6, 10, 9, 10}));
+//        System.out.println(lengthOfLIS(new int[]{1, 2, 4, 5}));
+//        System.out.println(lengthOfLIS(new int[]{1, 2, 6, 5}));
+//        System.out.println(lengthOfLIS(new int[]{4, 10, 4, 3, 8, 9}));
+        System.out.println(lengthOfLIS11(new int[]{4, 10, 4, 3, 8, 9}));
 
     }
 }
