a48bb16fdf4ede6cc123fc56ec4c6749b5f8dc90
zhouchen <jiaxue.pjx@alibaba-inc.com>
2020-12-28 23:52:52 +0800

    目标和

--- easy-leetcode/src/main/java/middle/dynamic/SubArray.java
+++ easy-leetcode/src/main/java/middle/dynamic/SubArray.java
@@ -77,10 +77,53 @@ public class SubArray {
 
     }
 
+    /**
+     * 给定一个非负整数数组，a1, a2, ..., an, 和一个目标数，S。现在你有两个符号 + 和 -。对于数组中的任意一个整数，你都可以从 + 或 -中选择一个符号添加在前面。
+     * <p>
+     * 返回可以使最终数组和为目标数 S 的所有添加符号的方法数。
+     *
+     * @param nums
+     * @param S
+     * @return
+     */
+    public static int findTargetSumWays(int[] nums, int S) {
+        int sum = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        if (sum < S || (sum + S) % 2 == 1) {
+            return 0;
+        }
+        return subsets(nums, (sum + S) / 2);
+
+    }
+
+
+    public static int subsets(int[] nums, int sum) {
+        int n = nums.length;
+        int[][] dp = new int[n + 1][sum + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
     public static void main(String[] args) {
         System.out.println(maxSubArray1(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
         System.out.println(maxSubArray2(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
         System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
     }
 
 
