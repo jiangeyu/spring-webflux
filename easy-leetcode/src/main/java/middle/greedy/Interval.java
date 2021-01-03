33bd9afad08427e9221c00025cd704103d97921e
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-03 14:14:39 +0800

    区间调度

--- easy-leetcode/src/main/java/middle/greedy/Interval.java
+++ easy-leetcode/src/main/java/middle/greedy/Interval.java
@@ -29,13 +29,49 @@ public class Interval {
         return length - count;
     }
 
+    /**
+     * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+     * <p>
+     * 输入:nums = [1,1,1], k = 2
+     * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+
+    public static int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] preSum = new int[n + 1];
+        int count = 0;
+        preSum[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            preSum[i] = nums[i - 1] + preSum[i - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (preSum[j] - nums[j-1] == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
     public static void main(String[] args) {
 //        System.out.println(eraseOverlapIntervals(new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 3}}));
 //        System.out.println(eraseOverlapIntervals(new int[][]{{1, 2}, {1, 2}, {1, 2}}));
 //        System.out.println(eraseOverlapIntervals(new int[][]{{1, 2}}));
 //        System.out.println(eraseOverlapIntervals(new int[][]{{1, 2},{2,3}}));
-        System.out.println(eraseOverlapIntervals(new int[][]{{1, 100}, {11, 22}, {1, 11}, {2, 12}}));
-        System.out.println(eraseOverlapIntervals(new int[][]{{1, 2}, {2, 3}, {3, 4}, {-100, -2}, {5, 7}}));
+//        System.out.println(eraseOverlapIntervals(new int[][]{{1, 100}, {11, 22}, {1, 11}, {2, 12}}));
+//        System.out.println(eraseOverlapIntervals(new int[][]{{1, 2}, {2, 3}, {3, 4}, {-100, -2}, {5, 7}}));
+        System.out.println(subarraySum(new int[]{1, 1, 1, 2}, 2));
+//        System.out.println(subarraySum(new int[]{0, 1, 1, 1, 2}, 2));
+        System.out.println(subarraySum(new int[]{1, 2, 3}, 3));
     }
 
 }
