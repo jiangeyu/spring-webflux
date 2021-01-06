cb78737ab3b0d0c0c0b0e43a7644b24dba6ea174
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-07 02:00:03 +0800

    跳跃有戏

--- easy-leetcode/src/main/java/middle/dynamic/Jump.java
+++ easy-leetcode/src/main/java/middle/dynamic/Jump.java
@@ -11,11 +11,42 @@ public class Jump {
         int further = 0;
         for (int i = 0; i < n - 1; i++) {
             further = Math.max(further, i + num[i]);
-            System.out.println(further);
             if (further <= i) {
                 return false;
             }
         }
         return further >= n - 1;
+    }
+
+
+    /**
+     * 给定一个非负整数数组，你最初位于数组的第一个位置。
+     * <p>
+     * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+     * <p>
+     * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+     *
+     * @param nums
+     * @return
+     */
+    public static int jump(int[] nums) {
+        int n = nums.length;
+        int end = 0;
+        int further = 0;
+        int jumps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            further = Math.max(further, i + nums[i]);
+            if (end == i) {
+                jumps++;
+                end = further;
+            }
+        }
+        return jumps;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,3,1,1,4};
+        System.out.println(jump(nums));
     }
 }
