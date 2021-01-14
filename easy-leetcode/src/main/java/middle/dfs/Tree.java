8c8218b74d89d5dcb63bc48ef3c4fd01e684dcb8
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-14 13:41:00 +0800

    dfs遍历

--- easy-leetcode/src/main/java/middle/dfs/Tree.java
+++ easy-leetcode/src/main/java/middle/dfs/Tree.java
@@ -11,14 +11,17 @@ import java.util.Queue;
 public class Tree {
 
 
+    /**
+     * 111. 二叉树的最小深度
+     *
+     * @param root
+     * @return
+     */
     public int minDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        if (root.left == null && root.right == null) {
-            return 1;
-        }
-        int minDepth = 0;
+        int minDepth = 1;
         Queue<TreeNode> queue = new ArrayDeque<>();
         queue.add(root);
         while (!queue.isEmpty()) {
@@ -38,5 +41,53 @@ public class Tree {
             minDepth++;
         }
         return minDepth;
+    }
+
+
+    /**
+     * 662
+     * <p>
+     * 给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。这个二叉树与满二叉树（full binary tree）结构相同，但一些节点为空。
+     * <p>
+     * 每一层的宽度被定义为两个端点（该层最左和最右的非空节点，两端点间的null节点也计入长度）之间的长度。
+     *
+     * @param root
+     * @return
+     */
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int minWidth = Integer.MIN_VALUE;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            minWidth = Math.max(minWidth, size);
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    break;
+                }
+                if (node.left == null) {
+                    queue.add(new TreeNode());
+                }
+                if (node.right == null) {
+                    queue.add(new TreeNode());
+                }
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return minWidth;
+
     }
 }
