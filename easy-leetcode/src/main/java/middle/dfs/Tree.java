adcf762250cd44589b76dd3c9121f1fd1551dcc8
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-14 13:17:42 +0800

    dfs遍历

---
+++ easy-leetcode/src/main/java/middle/dfs/Tree.java
@@ -0,0 +1,42 @@
+package middle.dfs;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
+ * @Description:
+ * @Date: Created in 下午12:35 2021/1/14
+ */
+public class Tree {
+
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int minDepth = 0;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return minDepth;
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            minDepth++;
+        }
+        return minDepth;
+    }
+}
---
+++ easy-leetcode/src/main/java/middle/dfs/TreeNode.java
@@ -0,0 +1,25 @@
+package middle.dfs;
+
+/**
+ * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
+ * @Description:
+ * @Date: Created in 下午12:35 2021/1/14
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
