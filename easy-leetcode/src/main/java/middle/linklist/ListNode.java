eaa365a34747d8f7f7849211b1198a6724088e26
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-15 11:15:29 +0800

    list

---
+++ easy-leetcode/src/main/java/middle/linklist/ListNode.java
@@ -0,0 +1,23 @@
+package middle.linklist;
+
+/**
+ * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
+ * @Description:
+ * @Date: Created in 下午11:50 2021/1/14
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ easy-leetcode/src/main/java/middle/linklist/Reverse.java
@@ -0,0 +1,275 @@
+package middle.linklist;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
+ * @Description:
+ * @Date: Created in 下午11:51 2021/1/14
+ */
+public class Reverse {
+
+    /**
+     * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+     * <p>
+     * k 是一个正整数，它的值小于或等于链表的长度。
+     * <p>
+     * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        ListNode curr = head;
+        ListNode next;
+        dummy.next = head;
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        for (int i = 0; i < length / k; i++) {
+            for (int j = 0; j < k - 1; j++) {
+                next = curr.next;
+                curr.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+            }
+            prev = curr;
+            curr = prev.next;
+        }
+        return dummy.next;
+    }
+
+    /**
+     *
+     * Leetcode 23
+     *
+     *
+     * 给你一个链表数组，每个链表都已经按升序排列。
+     *
+     * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+     *
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+        for (ListNode list : lists) {
+            if (list == null) {
+                continue;
+            }
+            pq.add(list);
+        }
+
+        while (!pq.isEmpty()) {
+            ListNode nextNode = pq.poll();
+            curr.next = nextNode;
+            curr = curr.next;
+            if (nextNode.next != null) {
+                pq.add(nextNode.next);
+            }
+        }
+        return dummyHead.next;
+
+    }
+
+    /**
+     * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+     * <p>
+     * 说明:
+     * 1 ≤ m ≤ n ≤ 链表长度。
+     *
+     * @param head
+     * @param m
+     * @param n
+     * @return
+     */
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        for (int i = 1; i < m; i++) {
+            pre = pre.next;
+        }
+        head = pre.next;
+        for (int i = m; i < n; i++) {
+            ListNode nex = head.next;
+            head.next = nex.next;
+            nex.next = pre.next;
+            pre.next = nex;
+        }
+        return dummy.next;
+    }
+
+    /**
+     *
+     * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+     *
+     * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+     * 24
+     *
+     * 使用递归来解决该题，主要就是递归的三部曲：
+     *
+     * 找终止条件：本题终止条件很明显，当递归到链表为空或者链表只剩一个元素的时候，没得交换了，自然就终止了。
+     * 找返回值：返回给上一层递归的值应该是已经交换完成后的子链表。
+     * 单次的过程：因为递归是重复做一样的事情，所以从宏观上考虑，只用考虑某一步是怎么完成的。我们假设待交换的俩节点分别为head和next，
+     * next的应该接受上一级返回的子链表(参考第2步)。就相当于是一个含三个节点的链表交换前两个节点，就很简单了，想不明白的画画图就ok。
+     * 如果独立写递归函数有困难的，可以参考一下我写的一个博客，附有详细的图文介绍：
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+
+
+    /**
+     * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+     * <p>
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+     * <p>
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode cursor = root;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sumVal = l1Val + l2Val + carry;
+            carry = sumVal / 10;
+
+            ListNode sumNode = new ListNode(sumVal % 10);
+            cursor.next = sumNode;
+            cursor = sumNode;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return root.next;
+    }
+
+
+    /**
+     * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+            if (l1 == null) {
+                cur.next = l2;
+
+            }
+            if (l2 == null) {
+                cur.next = l1;
+            }
+        }
+
+        return dummyHead.next;
+    }
+
+
+    /**
+     * 反转一个单链表。
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+    /**
+     * 反转一个单链表。
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseListIterative(ListNode head) {
+        ListNode prev = null; //前指针节点
+        ListNode curr = head; //当前指针节点
+        //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
+        while (curr != null) {
+            ListNode nextTemp = curr.next; //临时节点，暂存当前节点的下一节点，用于后移
+            curr.next = prev; //将当前节点指向它前面的节点
+            prev = curr; //前指针后移
+            curr = nextTemp; //当前指针后移
+        }
+        return prev;
+    }
+
+
+    ListNode temp;
+
+    /**
+     * 回文链表
+     *
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        temp = head;
+        return dfs(head);
+    }
+
+    public boolean dfs(ListNode head) {
+        if (head == null) return true;
+        boolean res = dfs(head.next) && temp.val == head.val;
+        temp = temp.next;
+        return res;
+    }
+}
+
+
