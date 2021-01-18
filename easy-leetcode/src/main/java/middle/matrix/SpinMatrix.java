7d8a40fdfa42eb44acfe6ceccf6b02d2d0354dcb
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-19 00:16:10 +0800

    合并区间

--- easy-leetcode/src/main/java/middle/matrix/SpinMatrix.java
+++ easy-leetcode/src/main/java/middle/matrix/SpinMatrix.java
@@ -1,6 +1,9 @@
 package middle.matrix;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
@@ -40,9 +43,9 @@ public class SpinMatrix {
     /**
      * 179
      * 给定一组非负整数 nums，重新排列它们每个数字的顺序（每个数字不可拆分）使之组成一个最大的整数。
-     *
+     * <p>
      * 注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
-     *输入：nums = [10,2]
+     * 输入：nums = [10,2]
      * 输出："210"
      *
      * @param nums
@@ -52,19 +55,45 @@ public class SpinMatrix {
         return Arrays.stream(nums)
                 .boxed()
                 .map(i -> i.toString(i))
-                .sorted((s1, s2)->{
+                .sorted((s1, s2) -> {
                     String sum1 = s1 + s2;
                     String sum2 = s2 + s1;
 
-                    for(int i = 0; i < sum1.length(); i++){
-                        if(sum1.charAt(i) != sum2.charAt(i)){
+                    for (int i = 0; i < sum1.length(); i++) {
+                        if (sum1.charAt(i) != sum2.charAt(i)) {
                             return sum2.charAt(i) - sum1.charAt(i);
                         }
                     }
                     return 0;
                 })
                 .reduce(String::concat)
-                .filter(s->!s.startsWith("0"))
+                .filter(s -> !s.startsWith("0"))
                 .orElse("0");
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1)
+            return intervals;
+        List<int[]> list = new ArrayList<>();
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        int i = 0;
+        int n = intervals.length;
+        while (i < n) {
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            while (i < n - 1 && right >= intervals[i + 1][0]) {
+                right = Math.max(right, intervals[i + 1][1]);
+                i++;
+            }
+            list.add(new int[]{left, right});
+            i++;
+        }
+        return list.toArray(new int[list.size()][2]);
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(merge(new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 3}}));
+//        System.out.println(merge(new int[][]{{1, 4}, {0, 4}}));
+        System.out.println(merge(new int[][]{{1, 4}, {4, 5}}));
     }
 }
