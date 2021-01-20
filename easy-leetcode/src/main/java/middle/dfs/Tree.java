3cf6476fca3c48083fa4a38b134b9d808682d5bc
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-20 19:53:26 +0800

    总路劲和

--- easy-leetcode/src/main/java/middle/dfs/Tree.java
+++ easy-leetcode/src/main/java/middle/dfs/Tree.java
@@ -184,4 +184,109 @@ public class Tree {
             return new ArrayList<>();
         return buildTrees(1,n);
     }
+
+
+    /**
+     * 226. 翻转二叉树
+     *
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        // 前序遍历反转即可
+        preorder(root);
+        return root;
+    }
+
+    public void preorder(TreeNode node) {
+        if(node==null){
+            return;
+        }
+        TreeNode left = node.left;
+        node.left = node.right;
+        node.right = left;
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+
+    /**
+     * 108. 将有序数组转换为二叉搜索树
+     *
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        // 左右等分建立左右子树，中间节点作为子树根节点，递归该过程
+        return nums == null ? null : buildTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildTree(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        int m = l + (r - l) / 2;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = buildTree(nums, l, m - 1);
+        root.right = buildTree(nums, m + 1, r);
+        return root;
+    }
+
+
+    List<String> ans = new ArrayList<>();
+
+    /**
+     * 257. 二叉树的所有路径
+     *
+     *
+     * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+     *
+     * 说明: 叶子节点是指没有子节点的节点。
+     * @param root
+     * @return
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        if(root == null){
+            return ans;
+        }
+        dfs(root, "");
+        return ans;
+    }
+
+    public void dfs(TreeNode root, String s){
+        s += root.val;  //拼接上新的节点值
+        if(root.left == null && root.right == null){
+            ans.add(s);
+        }
+        if(root.left != null){
+            dfs(root.left, s + "->");
+        }
+        if(root.right != null){
+            dfs(root.right, s + "->");
+        }
+    }
+
+    /**
+     *
+     * 112. 路径总和
+     *
+     * 定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+     *
+     * 说明: 叶子节点是指没有子节点的节点。
+     *
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum - root.val == 0;
+        }
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+    }
 }
