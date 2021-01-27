0d601baab29448af2b6f60c0c8bb156134fcce39
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-27 20:17:55 +0800

    倾斜容器

--- easy-leetcode/src/main/java/easy/SortStack.java
+++ easy-leetcode/src/main/java/easy/SortStack.java
@@ -1,9 +1,6 @@
 package easy;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @Author: <a href="mailto:">jiaxue.pjx@alibaba-inc.com</a>
@@ -213,6 +210,31 @@ public class SortStack {
                     vis[nx][ny] = true;
                 }
             }
+        }
+        return res;
+    }
+
+    /**
+     * 11
+     *
+     * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，
+     * 垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     *
+     * 说明：你不能倾斜容器。
+     *
+     *
+     *
+     * @param height
+     * @return
+     */
+    int maxArea(int[] height) {
+        if(height.length <= 1) return -1;
+        int i = 0, j = height.length - 1, res = 0;
+        while(i < j){
+            int h = Math.min(height[i], height[j]);
+            res = Math.max(res, h * (j - i));
+            if(height[i] < height[j]) ++i;
+            else --j;
         }
         return res;
     }
