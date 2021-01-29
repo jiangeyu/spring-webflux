cc02526a7de932ecf4545b8af68c46ace21954f3
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-29 17:21:25 +0800

    238

--- easy-leetcode/src/main/java/middle/dynamic/SubSequence.java
+++ easy-leetcode/src/main/java/middle/dynamic/SubSequence.java
@@ -106,6 +106,39 @@ public class SubSequence {
     }
 
     /**
+     * 718
+     * 给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组的长度。
+     *
+     * 最长公共子数组
+     *
+     * @param A
+     * @param B
+     * @return
+     */
+    public int findLength(int[] A, int[] B) {
+        int n = A.length;
+        int m = B.length;
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+        int result = 0;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    result = Math.max(result, dp[i][j]);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
      * 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
      * <p>
      * 你可以对一个单词进行如下三种操作：
