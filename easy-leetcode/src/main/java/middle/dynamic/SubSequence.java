0faf2ffb733d6f2c9576183e804f539a0135de08
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-01-31 17:16:13 +0800

    子序列

--- easy-leetcode/src/main/java/middle/dynamic/SubSequence.java
+++ easy-leetcode/src/main/java/middle/dynamic/SubSequence.java
@@ -11,15 +11,24 @@ public class SubSequence {
 
 
     /**
+     * 392. 判断子序列
+     *
      * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
      * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。
      * （例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
      */
     public boolean isSubsequence(String s, String t) {
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+        int i = 0, j = 0;
+        while (i < chars1.length && j < chars2.length) {
+            if (chars1[i] == chars2[j]) {
+                i++;
+            }
+            j++;
 
-
-        return false;
-
+        }
+        return i == chars1.length;
 
     }
 
