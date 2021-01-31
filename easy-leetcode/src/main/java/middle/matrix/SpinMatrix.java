9103656a5a6222b51b74b9e808acda5451ba7d25
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-02-01 01:26:10 +0800

    螺旋矩阵

--- easy-leetcode/src/main/java/middle/matrix/SpinMatrix.java
+++ easy-leetcode/src/main/java/middle/matrix/SpinMatrix.java
@@ -40,6 +40,49 @@ public class SpinMatrix {
         return arr;
     }
 
+//    if(matrix.empty() || matrix[0].empty()) return {};
+//    vector<int> res;
+//    int m = matrix.size(), n = matrix[0].size();
+//    // 确定上下左右四条边的位置
+//    int up = 0, down = m - 1, left = 0, right = n - 1;
+//        while (true)
+//    {
+//        for (int i = left; i <= right; i++) res.push_back(matrix[up][i]);
+//        if (++up > down) break;
+//        for (int i = up; i <= down; i++) res.push_back(matrix[i][right]);
+//        if (--right < left) break;
+//        for (int i = right; i >= left; i--) res.push_back(matrix[down][i]);
+//        if (--down < up) break;
+//        for (int i = down; i >= up; i--) res.push_back(matrix[i][left]);
+//        if (++left > right) break;
+//    }
+//        return res;
+
+    /**
+     * 54. 螺旋矩阵
+     * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+     *
+     * @param matrix
+     * @return
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int up = 0, down = m - 1, left = 0, right = n - 1;
+        List<Integer> list = new ArrayList<>();
+        while (true) {
+            for (int i = left; i <= right; i++) list.add(matrix[up][i]);
+            if (++up > down) break;
+            for (int i = up; i <= down; i++) list.add(matrix[i][right]);
+            if (--right < left) break;
+            for (int i = right; i >= left; i--) list.add(matrix[down][i]);
+            if (--down < up) break;
+            for (int i = down; i >= up; i--) list.add(matrix[i][left]);
+            if (++left > right) break;
+        }
+        return list;
+    }
+
     /**
      * 179
      * 给定一组非负整数 nums，重新排列它们每个数字的顺序（每个数字不可拆分）使之组成一个最大的整数。
