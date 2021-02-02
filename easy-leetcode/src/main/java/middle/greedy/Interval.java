b16147e7ca1f961899236b113b296917664d268b
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-02-03 00:03:35 +0800

    合并区间

--- easy-leetcode/src/main/java/middle/greedy/Interval.java
+++ easy-leetcode/src/main/java/middle/greedy/Interval.java
@@ -103,10 +103,10 @@ public class Interval {
      * 给出一个无重叠的 ，按照区间起始端点排序的区间列表。
      * <p>
      * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
-     *
+     * <p>
      * 输入：intervals = [[1,3],[6,9]], newInterval = [2,5]
      * 输出：[[1,5],[6,9]]
-     *
+     * <p>
      * 输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
      * 输出：[[1,2],[3,10],[12,16]]
      * 解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。
@@ -125,8 +125,11 @@ public class Interval {
                 res.add(cur);
             } else {
                 int[] last = res.get(res.size() - 1);
-                if (cur[0] > last[1]) res.add(cur);
-                else last[1] = Math.max(last[1], cur[1]);
+                if (cur[0] > last[1]) {
+                    res.add(cur);
+                } else {
+                    last[1] = Math.max(last[1], cur[1]);
+                }
             }
         }
         return res.toArray(new int[][]{});
