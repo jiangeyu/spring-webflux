ebe8c1c2e89c7e26d03967a5ccc0d53419537d9e
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-02-12 22:59:54 +0800

    计数排序由

--- easy-leetcode/src/main/java/middle/dynamic/SubArray.java
+++ easy-leetcode/src/main/java/middle/dynamic/SubArray.java
@@ -315,6 +315,15 @@ public class SubArray {
         return x == reversed || x == reversed / 10;
     }
 
+    public int reverse(int x) {
+        long n = 0;
+        while(x != 0) {
+            n = n*10 + x%10;
+            x = x/10;
+        }
+        return (int)n==n? (int)n:0;
+    }
+
     /**
      * 229. 求众数 II
      * <p>
