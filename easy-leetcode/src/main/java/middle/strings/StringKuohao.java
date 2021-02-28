f35ff939ad3df25deccd92b5fe95bc32e447837d
zhouchen <jiaxue.pjx@alibaba-inc.com>
2021-02-28 22:14:01 +0800

    两数相加

--- easy-leetcode/src/main/java/middle/strings/StringKuohao.java
+++ easy-leetcode/src/main/java/middle/strings/StringKuohao.java
@@ -71,6 +71,97 @@ public class StringKuohao {
         return String.valueOf(chars);
     }
 
+    /**
+     * 13. 罗马数字转整数
+     *
+     * @param s
+     * @return
+     */
+    public int romanToInt(String s) {
+        int n = s.length();
+        int roman_int = 0;
+        for(int i=0;i<n;i++)
+        {
+            switch(s.charAt(i))
+            {
+                case 'I' : roman_int = roman_int + 1;break;
+                case 'V' : roman_int = roman_int + 5;break;
+                case 'X' : roman_int = roman_int + 10;break;
+                case 'L' : roman_int = roman_int + 50;break;
+                case 'C' : roman_int = roman_int + 100;break;
+                case 'D' : roman_int = roman_int + 500;break;
+                case 'M' : roman_int = roman_int + 1000;break;
+                default: System.out.println("default");break;
+            }
+
+            if(i!=0)
+            {
+                if(((s.charAt(i)=='V')||(s.charAt(i)=='X'))&&(s.charAt(i-1)=='I'))
+                    roman_int = roman_int-1*2;
+                if(((s.charAt(i)=='L')||(s.charAt(i)=='C'))&&(s.charAt(i-1)=='X'))
+                    roman_int = roman_int-10*2;
+                if(((s.charAt(i)=='D')||(s.charAt(i)=='M'))&&(s.charAt(i-1)=='C'))
+                    roman_int = roman_int-100*2;
+            }
+        }
+        return roman_int;
+    }
+
+    /**
+     * 38. 外观数列
+     *
+     * @param n
+     * @return
+     */
+    public String countAndSay(int n) {
+        String s = "1";
+        for(int i=2;i<=n;i++){
+            s = toConver(s);
+        }
+        return s;
+    }
+
+    String toConver(String s){
+        int len = s.length();
+        int n=0;
+        StringBuilder sb = new StringBuilder();
+        char c = s.charAt(0);
+        int i=0;
+        while(i<len){
+            if(c == s.charAt(i)){
+                n++;
+                i++;
+            }else{
+                sb.append(n).append(c);
+                n=1;
+                c=s.charAt(i);
+                i++;
+            }
+        }
+        sb.append(n).append(c);
+        return sb.toString();
+    }
+
+    /**
+     * 415. 字符串相加
+     *
+     * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0, i = num1.length()-1, j = num2.length()-1;
+        while(i >= 0 || j >= 0 || carry != 0){
+            if(i>=0) carry += num1.charAt(i--)-'0';
+            if(j>=0) carry += num2.charAt(j--)-'0';
+            sb.append(carry%10);
+            carry /= 10;
+        }
+        return sb.reverse().toString();
+    }
+
     public static void main(String[] args) {
         System.out.println(generateParenthesis(3));
         System.out.println(convert("PAYPALISHIRING", 3));
